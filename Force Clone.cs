3a199870f3ca5e67e1a8f4fedfc026c34109d620
kaaku <92543349+sankaaku@users.noreply.github.com>
2021-10-15 15:37:41 -0700

    Add files via upload

---
+++ Force Clone.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+using VRC;
+
+[assembly: MelonInfo(typeof(ForceClone.Main), "Force Clone", "1", "kaaku")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+
+namespace ForceClone
+{
+    class Main : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            try
+            {
+                foreach (var player in PlayerManager.prop_PlayerManager_0.field_Private_List_1_Player_0)
+                {
+                    if (player.prop_APIUser_0.allowAvatarCopying.Equals(false))
+                    {
+                        player.prop_APIUser_0.allowAvatarCopying = true;
+                    }
+                }
+            }
+            catch (System.NullReferenceException) { };
+        }
+    }
+}
